da23e63e3edd73e04a856e99331d821d843739a1
Averianov <averianovai@gmail.com>
2021-01-07 14:56:47 +0300

    add defer

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"authservice/controllers"
+	"authservice/models"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := run()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func run() (err error) {
+	err = godotenv.Load()
+	if err != nil {
+		return
+	}
+	domain := os.Getenv("domain")
+	tokenPassword := os.Getenv("token_password")
+	urlDB := os.Getenv("url_db")
+
+	err = models.InitConnectionToDB(urlDB, tokenPassword)
+	if err != nil {
+		return
+	}
+
+	session := models.NewSession(models.NewAccount())
+	controller := controllers.NewAuthController(&session.Account, session, domain)
+	http.HandleFunc("/auth/login", controller.Authenticate)
+	http.HandleFunc("/auth/refresh", controller.Refresh)
+
+	http.ListenAndServe(domain+":8080", nil)
+	return
+}
